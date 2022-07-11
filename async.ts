bfbff837592c74d89cce2be47fe85c0d93034e5a
saltyaom <saltyaom@gmail.com>
2022-07-11 11:47:33 +0700

    :blue_book: doc: add not defers example

--- async.ts
+++ async.ts
@@ -1,6 +1,15 @@
+// ? Will not defers
+// const handle = async () => new Response("Hi")
+
+// export default {
+//   port: 8081,
+//   fetch: handle
+// }
+
+// ? Will Defers
 const handle = async () => new Response("Hi")
 
 export default {
   port: 8081,
-  fetch: async () => await handle()
+  fetch: async () => await handle
 }
